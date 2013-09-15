b795db503235a8f64da61cb80e485b571e004681
Babak Mansouri <mansouri@g.hardvard.edu>
2013-09-15 14:10:51 -0400

    adding Bio and code editor changes plus css

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>Harvard Extension School p1.dwa1.biz</title>
+  <title>Project 1 :: Dynamic Web Applications :: Babak Mansouri</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="This is a website created by Babak Mansouri for project submission for Dynamic Web Application Course at Harward Extension School ">
+  <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-Welcome to P1!
+
+<div id="main">  
+    <h1>  Babak Mansouri</h1>  
+	<p> <strong>Small Bio: </strong> I live in Washington DC metro area and work as a systems engineer. I have a software engineering degree and have been on and off coding since 1993. My last attempt to become a full time application developer goes back to 2003 that I worked as a JSP/J2EE developer for a year. Around the same time I developed a few PHP/MySQL websites in my spare time. And that was it! As I don’t want to let go of my passion I use VBScript, PowerShell and Wise Script for logon scripts and maintaining systems and servers since it makes my job much easier.</p>
+
+	<p>I am very excited about this course and want to learn Ajax/jQuery/Git/PHP. I want to pursue a graduate degree in Information Management Systems at Harvard Extension School and this is the first course I am taking.</p>
+
+	<p>In my spare time I spend time with my two daughters (6 and 2 years old), take my bicycle for a ride and if anytime is left I play guitar.</p> 
+ 
+ 	<p> <strong>Text Editor: </strong> Sublime Text 2 - and I love it.</p>
+
+</div>  
 
 </body>
 </html>
